fef74d5a455dc27ef6858c3aff0c6bcedf8b79e3
ColbyRansom <166167016+ColbyRansom@users.noreply.github.com>
2026-01-19 07:39:19 +0000

    Lab01 complete

---
+++ REPORT.md
@@ -0,0 +1,3 @@
+I added 2 additional tests to the lab01_tb.v file. These two tests changed the value of tick to 5hz and 50hz for each test repectively. This causes the program to test if the gen_tick module works for different output frequencies by changing how often tick changes.
+
+![alt text](<Screenshot 2026-01-18 232556.png>)
--- gen_tick.v
+++ gen_tick.v
@@ -1,11 +1,11 @@
 //=========================================================================
 // Name & Email must be EXACTLY as in Gradescope roster!
-// Name: 
-// Email: 
+// Name: Colby Ransom
+// Email: crans006@ucr.edu
 // 
-// Assignment name: 
-// Lab section: 
-// TA: 
+// Assignment name: Lab01-Clocks and Test Benches
+// Lab section: 01
+// TA: Allan Knight
 // 
 // I hereby certify that I have not received assistance on this assignment,
 // or used code, from ANY outside source other than the instruction team
@@ -19,13 +19,25 @@ module gen_tick # ( parameter SRC_FREQ = 5000, parameter TICK_FREQ = 1) (
     output tick
 );
 
-// Declare registers and wires here
 
+// Declare registers and wires here
+integer limit = SRC_FREQ / TICK_FREQ / 2 - 1;
+integer acc = 0;
+reg tick_out = 0;
 always @(posedge src_clk) begin
-    // put your code for the multiplier here
+    if (enable) begin
+        acc <= acc + 1;
+        if (acc >= limit) begin
+            tick_out <= ~tick_out;
+            acc <= 0;
+        end
+    end else begin
+        tick_out <= 0;
+    end
 end
 
+
 // Change this assign statement to the actual tick value
-assign tick = src_clk;
+assign tick = tick_out;
 
 endmodule
